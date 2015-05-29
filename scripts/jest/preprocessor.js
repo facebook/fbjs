e5708dba886ef350d810c6b6f2c5f13dfc8ebcc1
Paul O’Shannessy <paul@oshannessy.com>
2015-05-29 09:54:00 -0700

    Start running tests.
    Some currently fail because we expect Set to exist. It will at some
    point.

---
+++ scripts/jest/preprocessor.js
@@ -0,0 +1,25 @@
+// TODO: sync babel config with gulpfile. There are differences (eg, we don't
+// want to use the DEV plugin).
+
+var babel = require('babel');
+var babelPluginRequires = require('../babel/rewrite-requires');
+var assign = require('object-assign');
+
+var babelOpts = {
+  nonStandard: true,
+  optional: [
+    'es7.trailingFunctionCommas'
+  ],
+  plugins: [babelPluginRequires]
+};
+
+module.exports = {
+  process: function(src, path) {
+    // TODO: Use preprocessorIgnorePatterns when it works.
+    // See https://github.com/facebook/jest/issues/385.
+    if (!path.match(/\/node_modules\//) && !path.match(/\/third_party\//)) {
+      return babel.transform(src, assign({filename: path}, babelOpts)).code;
+    }
+    return src;
+  }
+};
