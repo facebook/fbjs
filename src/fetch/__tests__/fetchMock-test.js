4ae1706cdfaea88217c3d2e4b17e9964b4f15b27
Timothy Yung <yungsters@fb.com>
2015-07-22 17:01:37 -0700

    Add `fetchWithRetries` and `fetch` polyfill

---
+++ src/fetch/__tests__/fetchMock-test.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2013-2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+'use strict';
+
+describe('fetchMock', () => {
+  var Deferred;
+
+  var fetch;
+
+  beforeEach(() => {
+    Deferred = require('Deferred');
+    fetch = require('fetch');
+  });
+
+  it('has a corresponding `Deferred` for each call to `fetch`', () => {
+    expect(fetch.mock.calls.length).toBe(0);
+    expect(fetch.mock.deferreds.length).toBe(0);
+    var promise = fetch('//localhost', {});
+    expect(fetch.mock.calls.length).toBe(1);
+    expect(fetch.mock.deferreds.length).toBe(1);
+    var deferred = fetch.mock.deferreds[0];
+    expect(deferred instanceof Deferred).toBe(true);
+    var mockCallback = jest.genMockFunction();
+    var mockResult = {};
+    expect(mockCallback).not.toBeCalled();
+    promise.then(mockCallback);
+    deferred.resolve(mockResult);
+    jest.runAllTimers();
+    expect(mockCallback).toBeCalledWith(mockResult);
+  });
+
+});
