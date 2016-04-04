1f0e26c471d2c4e6979743c82ab77233ecfdc572
Paul O’Shannessy <paul@oshannessy.com>
2016-04-04 16:03:09 -0700

    Update babel 6 dev expression test

--- scripts/babel-6/__tests__/dev-expression.js
+++ scripts/babel-6/__tests__/dev-expression.js
@@ -50,14 +50,14 @@ if (process.env.NODE_ENV !== 'production') {
   it('should replace warning calls', () => {
     compare(
       `warning(condition, 'a %s b', 'c');`,
-      `process.env.NODE_ENV !== 'production' ? warning(condition, 'a %s b', 'c') : undefined;`
+      `process.env.NODE_ENV !== 'production' ? warning(condition, 'a %s b', 'c') : void 0;`
     );
   });
 
   it('should replace invariant calls', () => {
     compare(
       `invariant(condition, 'a %s b', 'c');`,
-      `!condition ? process.env.NODE_ENV !== 'production' ? invariant(false, 'a %s b', 'c') : invariant(false) : undefined;`
+      `!condition ? process.env.NODE_ENV !== 'production' ? invariant(false, 'a %s b', 'c') : invariant(false) : void 0;`
     );
   });
 });
