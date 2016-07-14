999e2ffa362484dbea48a10034f0773637d6d682
Paul O’Shannessy <paul@oshannessy.com>
2016-07-14 12:35:20 -0700

    [fbjs-css-vars] Initial release (#162)

---
+++ packages/fbjs-css-vars/CHANGELOG.md
@@ -0,0 +1,4 @@
+## [1.0.0] - 2016-07-14
+
+### Added
+- Initial release with variables used by Fixed Data Table and Draft.
---
+++ packages/fbjs-css-vars/index.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+module.exports = {
+  'fbui-desktop-background-light': '#f6f7f8',
+  'fbui-desktop-text-placeholder': '#9197a3',
+  'fbui-desktop-text-placeholder-focused': '#bdc1c9',
+  'fbui-white': '#fff',
+  'scrollbar-face-active-color': '#7d7d7d',
+  'scrollbar-face-color': '#c2c2c2',
+  'scrollbar-face-margin': '4px',
+  'scrollbar-face-radius': '6px',
+  'scrollbar-size': '15px',
+  'scrollbar-size-large': '17px',
+  'scrollbar-track-color': 'rgba(255, 255, 255, 0.8)',
+};
