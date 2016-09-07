2c6edc4d3725d14b7673387e5d9a8704c0bbcf40
Gabe Levi <gabe@fb.com>
2016-09-07 14:49:32 -0400

    Get rid of Flow suppress_type

--- packages/fbjs/src/core/shallowEqual.js
+++ packages/fbjs/src/core/shallowEqual.js
@@ -25,7 +25,8 @@ function is(x: mixed, y: mixed): boolean {
   // SameValue algorithm
   if (x === y) { // Steps 1-5, 7-10
     // Steps 6.b-6.e: +0 != -0
-    return x !== 0 || 1 / (x: $FlowIssue) === 1 / (y: $FlowIssue);
+    // Added the nonzero y check to make Flow happy, but it is redundant
+    return x !== 0 || y !== 0 || 1 / x === 1 / y;
   } else {
     // Step 6.a: NaN == NaN
     return x !== x && y !== y;
