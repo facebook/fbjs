d92d408d400dbb1a4ebb078f5e85f7ac3c8d41b7
Leonardo Quixadá <lquixada@gmail.com>
2017-10-17 23:09:31 -0200

    Added a more updated alternative to isomorphic-fetch.

--- packages/fbjs/src/__forks__/fetch.js
+++ packages/fbjs/src/__forks__/fetch.js
@@ -9,11 +9,8 @@
 
 'use strict';
 
-// This hopefully supports the React Native case, which is already bringing along
-// its own fetch polyfill. That should exist on `global`. If that doesn't exist
-// then we'll try to polyfill, which might not work correctly in all environments.
-if (global.fetch) {
-  module.exports = global.fetch.bind(global);
-} else {
-  module.exports = require('isomorphic-fetch');
-}
+// Add a universal fetch polyfill (React Native compatible)
+require('cross-fetch/polyfill')
+
+// Make it importable
+module.exports = global.fetch.bind(global);
