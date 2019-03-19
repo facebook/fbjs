af9fefad110a04821f71593b17263e8dece089cb
Matt Hargett <plaztiksyke@gmail.com>
2019-03-19 09:42:21 -0700

    Align versions to other facebook repos (#343)
    Align babel and eslint package versions to metro and react-native, to eliminate peer dependency warnings and an npm de-duplication problem.
    
    Main repo didn't lint or prettier cleanly for me. I ran the prettier yarn target, but left the lint warnings alone.
    
    The packages/* versions need to be bumped as well, but wasn't sure what the policy was. Tell me and I'll change them.
    
    Some additional notes:
    1. I tried bumping babel-plugin-tester to 5.6.x to eliminate another npm warning, but got some unhelpful test failure messages that I didn't have time to debug:
    ```
     FAIL  plugins/__tests__/inline-requires-test.js
      ● inline-requires › ignores requires that are re-assigned
    
        AssertionError [ERR_ASSERTION]: Expected output to not change, but it did
    
          at tester (node_modules/babel-plugin-tester/dist/index.js:197:27)
          at Object.<anonymous> (node_modules/babel-plugin-tester/dist/index.js:143:13)
          at asyncGeneratorStep (node_modules/babel-plugin-tester/dist/index.js:21:103)
          at _next (node_modules/babel-plugin-tester/dist/index.js:23:194)
    
      ● inline-requires › ignores destructured properties of requires in `ignoredRequires`
    
        AssertionError [ERR_ASSERTION]: Expected output to not change, but it did
    ```
    
    2. I tried bumping to Jest 24.6 like the other facebook repos, but ran into needed test changes because of require mocking. I'm deferring that to a different PR.

--- packages/babel-preset-fbjs/plugins/inline-requires.js
+++ packages/babel-preset-fbjs/plugins/inline-requires.js
@@ -160,12 +160,16 @@ function isInlineableCall(node, state) {
     node['arguments'][0].type === 'CallExpression' &&
     node['arguments'][0].callee.type === 'MemberExpression' &&
     node['arguments'][0].callee.object.type === 'Identifier' &&
-    state.inlineableCalls.hasOwnProperty(node['arguments'][0].callee.object.name) &&
+    state.inlineableCalls.hasOwnProperty(
+      node['arguments'][0].callee.object.name
+    ) &&
     node['arguments'][0].callee.property.type === 'Identifier' &&
     node['arguments'][0].callee.property.name === 'resolve' &&
     node['arguments'][0]['arguments'].length >= 1 &&
     node['arguments'][0]['arguments'][0].type === 'StringLiteral' &&
-    !state.ignoredRequires.hasOwnProperty(node['arguments'][0]['arguments'][0].value);
+    !state.ignoredRequires.hasOwnProperty(
+      node['arguments'][0]['arguments'][0].value
+    );
 
   return isStandardCall || isRequireResolveCall;
 }
