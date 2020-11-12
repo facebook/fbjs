d613c120a4eaad80e29af0b216f8767bf2b5bffb
George Zahariev <gkz@fb.com>
2020-11-11 17:56:10 -0800

    Don't use unsupported syntax

--- packages/fbjs/src/key-mirror/keyMirrorRecursive.js
+++ packages/fbjs/src/key-mirror/keyMirrorRecursive.js
@@ -29,7 +29,7 @@ var invariant = require('invariant');
  *   var CONSTANTS = keyMirror({FOO: {BAR: null}}, 'NameSpace');
  *   console.log(CONSTANTS.FOO.BAR); // NameSpace.FOO.BAR
  */
-function keyMirrorRecursive<T: {...}>(obj: T, prefix?: ?string): T {
+function keyMirrorRecursive<T: {}>(obj: T, prefix?: ?string): T {
   // $FlowFixMe
   return keyMirrorRecursiveInternal(obj, prefix);
 }
