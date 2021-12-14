e3e8d689b0c1f114b17fd0179310117e5b110746
Jon Janzen <jjjonjanzen@gmail.com>
2021-12-14 09:07:20 -0800

    Fix @flow weak usage (#469)
    Co-authored-by: Marshall Roch <mroch@fb.com>

--- packages/fbjs/src/key-mirror/keyMirrorRecursive.js
+++ packages/fbjs/src/key-mirror/keyMirrorRecursive.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  *
  * @providesModule keyMirrorRecursive
- * @flow weak
+ * @flow strict
  * @typechecks
  */
 
@@ -13,59 +13,67 @@
 
 var invariant = require('invariant');
 
+export type KeyMirrorRecursive<O> = $ObjMapi<
+  O,
+  (<O: {...}>(mixed, O) => KeyMirrorRecursive<O>) & (<K>(K) => K),
+>;
+
+export type KeyMirrorRecursiveWithPrefix<O> = $ObjMapi<
+  O,
+  (<O: {...}>(mixed, O) => KeyMirrorRecursiveWithPrefix<O>) & (mixed => string),
+>;
+
+type keyMirrorRecursiveFn = (<O: {...}>(
+  obj: O,
+  _: void,
+) => KeyMirrorRecursive<O>) &
+  (<O: {...}>(obj: O, prefix: string) => KeyMirrorRecursiveWithPrefix<O>);
+
 /**
  * Constructs an enumeration with keys equal to their value. If the value is an
  * object, the method is run recursively, including the parent key as a suffix.
- * An optional prefix can be provided that will be prepended to each value.
+ * An optional prefix can be provided that will be prepended to each value, but
+ * note that if a prefix is provided, the types the values of the object are
+ * just strings, not string literals.
  *
  * For example:
  *
- *   var ACTIONS = keyMirror({FOO: null, BAR: { BAZ: null, BOZ: null }}});
+ *   var ACTIONS = keyMirror({FOO: '', BAR: { BAZ: '', BOZ: '' }}});
  *   ACTIONS.BAR.BAZ = 'BAR.BAZ';
  *
- *   Input:  {key1: null, key2: { nested1: null, nested2: null }}}
+ *   Input:  {key1: '', key2: { nested1: '', nested2: '' }}}
  *   Output: {key1: key1, key2: { nested1: nested1, nested2: nested2 }}}
  *
- *   var CONSTANTS = keyMirror({FOO: {BAR: null}}, 'NameSpace');
+ *   var CONSTANTS = keyMirror({FOO: {BAR: ''}}, 'NameSpace');
  *   console.log(CONSTANTS.FOO.BAR); // NameSpace.FOO.BAR
  */
-function keyMirrorRecursive<T>(obj: T, prefix?: ?string): T {
-  return keyMirrorRecursiveInternal(obj, prefix);
-}
-
-function keyMirrorRecursiveInternal(
-  /*object*/ obj,
-  /*?string*/ prefix
-) /*object*/ {
-  var ret = {};
-  var key;
-
+const keyMirrorRecursive: keyMirrorRecursiveFn = (obj: {...}, prefix) => {
+  const ret: {[string]: KeyMirrorRecursiveWithPrefix<$FlowFixMe> | string} = {};
   invariant(
     isObject(obj),
-    'keyMirrorRecursive(...): Argument must be an object.'
+    'keyMirrorRecursive(...): Argument must be an object.',
   );
 
-  for (key in obj) {
+  for (const key in obj) {
     if (!obj.hasOwnProperty(key)) {
       continue;
     }
 
     var val = obj[key];
-
-    var newPrefix = prefix ? prefix + '.' + key : key;
-
+    var newPrefix = (prefix != null && Boolean(prefix)) ? prefix + '.' + key : key;
     if (isObject(val)) {
-      val = keyMirrorRecursiveInternal(val, newPrefix);
+      val = keyMirrorRecursive(val, newPrefix);
     } else {
       val = newPrefix;
     }
 
     ret[key] = val;
   }
-  return ret;
-}
 
-function isObject(obj) /*boolean*/ {
+  return ret;
+};
+
+function isObject<T: {...}>(obj: T): boolean {
   return obj instanceof Object && !Array.isArray(obj);
 }
 
